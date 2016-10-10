eab784ed38981201028f7aa49e07bf945a776755
RobertWhite7 <robertwhite@RobertWhiteMBP.local>
2016-10-10 11:38:34 -0400

    added base classes

---
+++ TipCalculator-Raw/TipCalculator-Raw/Interactive.swift
@@ -0,0 +1,42 @@
+//
+//  Interactive.swift
+//  TipCalculator-Raw
+//
+//  Created by Robert White on 10/10/16.
+//  Copyright © 2016 Teky. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class Interactive {
+    private var done: Bool = false
+    private var currentInput: String = "q"
+    private var io = Io()
+    
+    func go() {
+        while !done {
+            
+            //Ask the user for input right here.
+            
+            io.writeMessage ("\nInput")
+            currentInput = io.getInput()
+            
+            if currentInput == "q" {
+                done = true
+            }else{
+                print("The input is: \(currentInput)")
+            }
+            
+        }
+        print ("Exiting....")
+    
+        return
+    }
+}
+let interactive = Interactive()
+
+// Run in interactive mode
+
---
+++ TipCalculator-Raw/TipCalculator-Raw/io.swift
@@ -0,0 +1,28 @@
+//
+//  io.swift
+//  TipCalculator-Raw
+//
+//  Created by Robert White on 10/10/16.
+//  Copyright © 2016 Teky. All rights reserved.
+//
+
+import Foundation
+
+class Io {
+    func getInput() -> String {
+        let keyboard = FileHandle.standardInput
+        let inputData = keyboard.availableData
+        
+        let str = NSString(data: inputData, encoding: String.Encoding.utf8.rawValue)
+        if let string = str {
+            return string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }else {
+            return "Invalid Input!"
+        }
+        
+    }
+    func writeMessage(_ message: String){
+        print("\(message)/n")
+    }
+    
+}
